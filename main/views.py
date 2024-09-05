26f4dab53bfde045a3ee4fc66729b1c2fbd04e29
Pauline Taillemite <paulinetaillemite@MacBook-Air-de-Pauline.local>
2024-09-05 15:44:15 +0200

    test connection git vercel

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import random
 # Create your views here.
+
+# Créer une fonction pour render fortune.html
+
+
+def test(request):
+    return render (request, "main/test.html")
+
+
