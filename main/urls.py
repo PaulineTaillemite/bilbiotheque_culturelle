6dd5bc54c7318f5b83a80f438ad13dfdc1c5c6bb
Pauline Taillemite <paulinetaillemite@air-de-pauline.home>
2024-09-10 15:26:48 +0200

    ajout lien

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns=[
     path("",views.test, name="test"),
-    path("test2",views.test2),
+    path("test2",views.test2, name="test2"),
     path("myprofile/",views.myprofile),
 
 ]
