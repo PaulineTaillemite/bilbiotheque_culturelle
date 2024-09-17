8bbed9c966e5a15817a60bc018a871b73cdb4567
Pauline Taillemite <paulinetaillemite@MacBook-Air-de-Pauline.local>
2024-09-17 14:45:55 +0200

    ajout dico

--- main/views.py
+++ main/views.py
@@ -4,8 +4,6 @@ import random
 # Create your views here.
 
 # Créer une fonction pour render fortune.html
-
-
 def test(request):
     return render (request, "main/test.html")
 
@@ -15,5 +13,19 @@ def myprofile(request):
 
 def test2(request):
     return render (request, "main/test2.html")
+
+#test dictionnaire
+videogames = [
+    {"name": "The Legend of Zelda: Breath of the Wild", "studio": "Nintendo", "year": 2017},
+    {"name": "The Legend of Zelda: Ocarina of Time", "studio": "Nintendo", "year": 1998},
+    {"name": "The Witcher 3: Wild Hunt", "studio": "CD Projekt Red", "year": 2015},
+    {"name": "Cyberpunk 2077", "studio": "CD Projekt Red", "year": 2020},
+    {"name": "Red Dead Redemption 2", "studio": "Rockstar Games", "year": 2018},
+    {"name": "Grand Theft Auto V", "studio": "Rockstar Games", "year": 2013},
+    {"name": "God of War", "studio": "Santa Monica Studio", "year": 2018},
+    {"name": "God of War: Ragnarok", "studio": "Santa Monica Studio", "year": 2022},
+    {"name": "Horizon Zero Dawn", "studio": "Guerrilla Games", "year": 2017},
+    {"name": "Horizon Forbidden West", "studio": "Guerrilla Games", "year": 2022},
+]
 
 
